1e240701316d1ed2f8df6ee9e88f71a0ce31e74d
samsonpaul <samsonpaul4@gmail.com>
2016-11-10 16:18:20 +0300

    More tests and house cleaning

---
+++ README.md
@@ -0,0 +1,8 @@
+#  Andela BootCamp 12
+## Andela Self Learning Clinic Day O
+
+This repository contains a senerio of Programming Logic, TDD, Git & Version Control based on the list below
+
+- A function to generate a list of prime numbers in the range(1, n)
+- Atleast 10 tests for the Function
+- And ofcos a repo for the same, here we are.
--- test/test_primeNumbers.py
+++ test/test_primeNumbers.py
@@ -9,14 +9,34 @@ class Prime_Numbers_TestCase(unittest.TestCase):
 	def test_case_integers(self):
 		self.assertEqual(primeNumbers.generate_prime_numbers(""), 'Please ensure that you pass an integer')
 
-	def test_case_primeNumbers(unittest.TestCase):
+	def test_case_primeNumbers(self):
 		self.assertEqual(primeNumbers.generate_prime_numbers(10), [2, 3, 5, 7])
 
-	
-	def test_case_Negatives(unittest.TestCase):
+	def test_case_Negatives(self):
 		self.assertEqual(primeNumbers.generate_prime_numbers(-6), [])
 
+	def test_case_primeNumbers(self):
+		self.assertEqual(primeNumbers.generate_prime_numbers(2), [2, 3, 5, 7, 11, 13, 17, 19])
 
-if __name__ == '__Prime_Numbers_Test__':
-	Prime_Numbers_TestCase()
+	def test_case_Errors(self):
+		self.assertEqual(primeNumbers.generate_prime_numbers(0) )
+
+	def test_case_string(self):
+		self.assertEqual(primeNumbers.generate_prime_numbers('2'), 'Please ensure that you pass an integer')
+
+	def test_case_list(self):
+		self.assertEqual(primeNumbers.generate_prime_numbers([3, 4, 60]), 'Please ensure that you pass an integer')
+
+	def test_case_primeNumbers(self):
+		self.assertEqual(primeNumbers.generate_prime_numbers(100), [[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]])
+
+	def test_case_primeNumbers(self):
+		self.assertEqual(primeNumbers.generate_prime_numbers(2), [1])
+
+	def test_case_primeList_Return(self):
+		self.assertIsInstance(primeNumbers.generate_prime_numbers(7), list)
+
+
+if __name__ == '__main__':
+	unittest.main()
 		
